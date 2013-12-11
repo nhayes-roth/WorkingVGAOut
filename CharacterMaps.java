ac5b596d401cd3a7ca73d003f3f5f48710003a5a
nhayes-roth <nathan.hayesroth@gmail.com>
2013-12-11 14:22:38 -0500

    screenfont.h split into Strings for each row

--- CharacterMaps.java
+++ CharacterMaps.java
@@ -19,11 +19,11 @@ class CharacterMaps {
 	/* Main Function */
 	public static void main (String[] args) throws IOException{
 		String code_page = readScreenFont(file_name);
-		System.out.println(code_page);
+		String[] string_array = stringToArray(code_page);
 	}
 
 	/*
-	 * Read in screenfont.h
+	 * Read in contents of screenfont.h to String.
 	 */
 	private static String readScreenFont(String file_name) throws IOException{
 		File file = new File(file_name);
@@ -42,6 +42,26 @@ class CharacterMaps {
 	}
 
 	/*
-	 * 
+	 * Convert String to String[8][256].
 	 */
+	private static String[] stringToArray(String code_page){
+		String [] rows 		= new String [8];				// array to be returned
+		String row 			= "";							// string of each row
+		String row_delim 	= " { \n  ";					// deliminates the start of a new row
+		int start_index 	= code_page.indexOf(row_delim);	// 
+		int end_index 		= start_index;
+		for (int i = 0; i < 8; i++){
+			end_index = code_page.indexOf("\n { ", start_index+1);
+			if (end_index == -1){
+				row = code_page.substring(start_index + 7, code_page.length()-37);
+			} else {
+				row = code_page.substring(start_index + 7, end_index - 17);
+			}
+			System.out.println("Row " + (i+1) + ": " + row.substring(0,5) + "..." + row.substring(row.length()-6));
+			rows[i] = row;
+			start_index = end_index;
+		}
+		return rows;
+
+	}
 }
